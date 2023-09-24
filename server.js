d114afcdb6630a9fcfba7497817f6952ca41f6a8
deaththekidd <ohiemidivine7@gmail.com>
2023-09-24 16:38:13 +0100

    update

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ var corsOptions = {
 };
 
 app.use(cors());
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
 
 // parse requests of content-type - application/json
 app.use(express.json());
