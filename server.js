a61fc0f47195c4136e9c42f4b8b1aeb7943af59d
deaththekidd <ohiemidivine7@gmail.com>
2023-09-24 19:48:09 +0100

    update

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const cookieSession = require("cookie-session");
 const app = express();
 
 app.use(cors());
-app.use(function (req, res, next) {
-	const origin = req.headers.origin;
-	if (allowedOrigins.includes(origin)) {
-		res.header("Access-Control-Allow-Origin", origin);
-	}
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
 
 // parse requests of content-type - application/json
 app.use(express.json());
